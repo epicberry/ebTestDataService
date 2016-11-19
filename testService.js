783c2d3864ec296ee0adb63f3fd92d2fbf24d9d4
Epic Berry <epicbrry@gmail.com>
2016-11-18 20:41:36 -0500

    integrated to MongoDB

--- testService.js
+++ testService.js
@@ -7,13 +7,25 @@ var restify = require('restify');
 //var testCases = sampleData.testCases;
 
 function getTestCaseData(req, res, next) {
-
     res.charSet('utf-8');
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
-    var testCaseDetails = fetchTestCaseDetails(req.params.testCaseId);
-    res.send(testCaseDetails);
+    // Data from DB
+    var testData = '';
+    GetTestDataFromDB(function(result){
+        testData = result;
+        res.send(testData);
+        next();
+    });
+
+    //For Hard-coded data uncomment the following
+    // var sampleTestCases = require('./sampleData.js');
+    // sampleTestCases.testCaseId = testCaseId;
+    // var testCaseDetails = sampleTestCases.testCaseDetails2(3);
+    // res.send(testCaseDetails);
+    // next();
+
 
     // if (req.params.testCaseId == 'all') {
     //     res.send(testCases);
@@ -24,29 +36,60 @@ function getTestCaseData(req, res, next) {
     // });
 
     // }
-    next();
 }
 
 function getTestCases(req, res, next) {
     res.charSet('utf-8');
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    var testCases = fetchTestCases();
-    res.send(testCases);
-    next();
+
+    // Data from DB
+    var testCases = '';
+    GetTestCasesFromDB(function(result){
+        testCases = result;
+        res.send(testCases);
+        next();
+    });
+
+    //For Hard-coded data uncomment the following
+    // var sampleTestCases = require('./sampleTestCases.js');
+    // res.send(sampleTestCases.testCases);
+    // next();
 }
 
-function fetchTestCases() {
-    // Replace with Ajax Call
-    var sampleTestCases = require('./sampleTestCases.js');
-    return sampleTestCases.testCases;
+// function GetDataFromDB(testQuery) {
+// var mongoose = require('mongoose');
+// mongoose.connect('mongodb://localhost:27017/autoDB');
+// }
+
+function GetTestCasesFromDB(callback) {
+var MongoClient = require('mongodb').MongoClient
+return MongoClient.connect('mongodb://localhost:27017/autoDB', function (err, db) {
+  if (err) throw err
+
+  // db.collection('testCases').find().toArray(function (err, result) {
+  db.collection('testCases').find({},{"_id":1,"testCaseId":1, "testCategory":1, "description": 1}).limit(10).sort({"_id":-1}).toArray(function (err, result) {
+    if (err) throw err
+
+    //console.log(result);
+    callback(result);
+  })
+})
 }
 
-function fetchTestCaseDetails(testCaseId) {
-    // Replace with Ajax Call
-    var sampleTestCases = require('./sampleData.js');
-    sampleTestCases.testCaseId = testCaseId;
-    return sampleTestCases.testCaseDetails2(3);
+function GetTestDataFromDB(callback) {
+var MongoClient = require('mongodb').MongoClient
+return MongoClient.connect('mongodb://localhost:27017/autoDB', function (err, db) {
+  if (err) throw err
+
+  // db.collection('testCases').find().toArray(function (err, result) {
+  db.collection('testCases').find({},{"_id":1,"testCaseId":1, "testCategory":1, "browser": 1, "testSteps": 1}).limit(10).sort({"_id":-1}).toArray(function (err, result) {
+    if (err) throw err
+
+    //console.log(result);
+    callback(result);
+  })
+})
 }
 
 var server = restify.createServer({
