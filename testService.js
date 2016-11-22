fa216d480882a2499d07a285cf7f09e2587b6797
Epic Berry <epicbrry@gmail.com>
2016-11-21 19:06:55 -0500

    updated remote MongoDB Connection

--- testService.js
+++ testService.js
@@ -65,7 +65,9 @@ function getTestCases(req, res, next) {
 function GetTestCasesFromDB(callback) {
 var MongoClient = require('mongodb').MongoClient
 // return MongoClient.connect('mongodb://localhost:27017/autoDB', function (err, db) {
-return MongoClient.connect('mongodb://192.169.179.82:27017/autoDB', function (err, db) {
+// return MongoClient.connect('mongodb://192.169.179.82:27017/autoDB', function (err, db) {
+return MongoClient.connect('mongodb://user1:Pa$$W0rd@ds143767.mlab.com:43767/autodb', function (err, db) {
+
   if (err) throw err
 
   // db.collection('testCases').find().toArray(function (err, result) {
@@ -80,7 +82,8 @@ return MongoClient.connect('mongodb://192.169.179.82:27017/autoDB', function (er
 
 function GetTestDataFromDB(callback) {
 var MongoClient = require('mongodb').MongoClient
-return MongoClient.connect('mongodb://localhost:27017/autoDB', function (err, db) {
+// return MongoClient.connect('mongodb://localhost:27017/autoDB', function (err, db) {
+return MongoClient.connect('mongodb://user1:Pa$$W0rd@ds143767.mlab.com:43767/autodb', function (err, db) {
   if (err) throw err
 
   // db.collection('testCases').find().toArray(function (err, result) {
